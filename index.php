8ecb4aa1dd83b93d72e63337a18d6d37a65b5b7a
Andrew Drane <andrew@andrewdrane.co>
2012-06-02 19:23:31 -0400

    adding templates

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+//Main presentation file. Acts as a controller for the rest of the files
+
+require 'includes/mustache/Mustache.php';
+
+
+/** Controller class
+ * 
+ */
+class Presentation {
+    
+    
+    
+}
+
+//routes the class
+class Router {
+    
+}
+
+//Static class for rendering presentation views
+class Render {
+    
+}
+
+echo 'hi';
---
+++ templates/main.mustache.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>{{title}}</title>
+    
+    <link rel="stylesheet" type="text/css" href="/style/stylesheets/screen.css" />
+    {{scripts}}
+</head>
+
+<body>
+    
+    
+    
+<!-- BEGIN HEADER -->
+    <div id="header_container">
+        <div id="header">
+
+        </div>
+    </div>
+<!-- END HEADER -->
+
+
+
+    <div id="container">
+        <div id="content">
+            
+            {{content}}
+            
+        </div>
+    </div>
+
+
+</body>
+
+</html>
